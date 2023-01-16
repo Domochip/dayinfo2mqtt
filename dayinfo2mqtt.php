1af3359c617ddb3785f9b06363dc0458916b6848
DomoChip <domochip@hotmail.com>
2023-01-16 19:30:51 +0100

    Bump version

--- dayinfo2mqtt.php
+++ dayinfo2mqtt.php
@@ -292,7 +292,7 @@ function publishSeason(MqttClient $mqtt, $prefix) {
 //------------------------------- MAIN -------------------------------
 //--------------------------------------------------------------------
 
-$versionnumber='1.0.8';
+$versionnumber='1.0.9';
 
 echo sprintf('===== dayinfo2mqtt v%s =====',$versionnumber).PHP_EOL;
 
